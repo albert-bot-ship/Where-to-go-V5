ae9ceb8c6d983fac8f714e839b000667d2e4e61e
albert-bot-ship <125125214+albert-bot-ship@users.noreply.github.com>
2026-04-19 02:01:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -163,14 +163,3 @@ function updateActive() {
     });
   }
 }
-
-document.querySelectorAll('.feature-card').forEach(card => {
-  card.addEventListener('mousemove', e => {
-    const rect = card.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    card.style.setProperty('--x', `${x}px`);
-    card.style.setProperty('--y', `${y}px`);
-  });
-});
